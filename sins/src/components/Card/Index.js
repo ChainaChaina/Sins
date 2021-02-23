c6179d31c4effac09c08d65a1c6ca2e621f62426
Lucas Borges Moraes <lucasborgesmoraes@gmail.com>
2021-02-23 17:54:29 -0300

    work day

--- sins/src/components/Card/Index.js
+++ sins/src/components/Card/Index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import '../Card/Style.css'
+import shortid from "shortid";
+
+
+
+function action(e) {
+
+  e.preventDefault();
+  console.log(e)
+  
+}
 
 
 function Card() {
   return <div>
-    <div className='cardBody'>
-    I love the smell of fresh plastic in my lungs in the morning.
+    <div className='cardBody animation' onClick={action} >
+      I love the smell of fresh plastic in my lungs in the morning.
     </div>
   </div>;
 }
